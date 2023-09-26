f78a2ad75a940d2eefbe3a3a8d06f794452def55
KB284 <khubaibfarah@gmail.com>
2023-09-26 14:54:28 -0400

    Script to download Youtube videos

---
+++ downToLoad.py
@@ -0,0 +1,21 @@
+import yt_dlp
+
+
+while True:
+    #enter url for download
+    url = input("Enter video url: ")
+
+    ydl_opts = {}
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    print("Video downloaded successfully!")
+
+    response = input(str("Would you like to keep downloading videos?(yes/no)"))
+
+    if response == "yes":
+        print("")
+    else:
+        print("Thank you for using this software.")
+        break
